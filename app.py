d63e358fe6f814ded43899dbc7fdc7f616fb95a2
Guy Marshall <guymarshall9@gmail.com>
2022-08-25 21:45:27 +0100

    Finish conversion

--- app.py
+++ app.py
@@ -1,50 +1,27 @@
-# There is a little one that comes to mind: if the Earth stopped in its orbit around the Sun, it would then
-# accelerate under gravity until it hit the Sun. Using Newton's equation for universal gravitation, you could
-# calculate the Earth's progress as it falls towards the Sun second by second.
-#
-# Yes, and for the acceleration (which is what you care about, not the force) you can remove the mass of the
-# Earth from that equation
-#
-# It will be similar to your car acceleration problem.
-#
-# Work out the acceleration, a = -GM/(r²) . Take this as constant for 1s = delta-t .
-#
-# The change in velocity, delta-v = a × delta-t .
-#
-# The change in displacement (distance from the Sun) in 1s, delta-s = v × delta-t .
-#
-# The new displacement, s = s-old + delta-s .
-# And similar to work out the new v.
-
 def main():
-    print("running")
+    gravitational_constant = 0.000000000066743
+    mass_of_sun_kg = 1988470000000000000000000000000.0
+    distance_between_earth_and_sun_m = 151480000000.0
+
+    delta_t = 1
+    time = 0
+    distance_from_sun = distance_between_earth_and_sun_m
+    velocity = 0.0
+
+    while distance_from_sun > 0.0:
+        acceleration = -1.0 * (gravitational_constant * mass_of_sun_kg) / (distance_from_sun * distance_from_sun)
+        delta_v = acceleration * delta_t
+        velocity += delta_v
+        delta_s = velocity * delta_t
+        distance_from_sun += delta_s
+
+        print(f"Time: {time}s,"
+              f"acceleration: {acceleration}ms^-2,"
+              f"velocity: {velocity}ms^-1,"
+              f"distance: {distance_from_sun}m")
+
+        time += delta_t
 
 
 if __name__ == '__main__':
     main()
-
-# fn main() {
-#     const GRAVITATIONAL_CONSTANT: f64 = 0.000000000066743;
-#     const MASS_OF_SUN_KG: f64 = 1988470000000000000000000000000.0;
-#     const DISTANCE_BETWEEN_EARTH_AND_SUN_M: f64 = 151480000000.0;
-#
-#     let delta_t: f64 = 1.0;
-#     let mut time: f64 = 0.0;
-#     let mut distance_from_sun: f64 = DISTANCE_BETWEEN_EARTH_AND_SUN_M;
-#     let mut acceleration: f64;
-#     let mut velocity: f64 = 0.0;
-#     let mut delta_v: f64;
-#     let mut delta_s: f64;
-#
-#     while distance_from_sun > 0.0 {
-#         acceleration = -1.0 * (GRAVITATIONAL_CONSTANT * MASS_OF_SUN_KG) / (distance_from_sun * distance_from_sun);
-#         delta_v = acceleration * delta_t;
-#         velocity += delta_v;
-#         delta_s = velocity * delta_t;
-#         distance_from_sun += delta_s;
-#
-#         println!("Time: {}s, acceleration: {}ms^-2, velocity: {}ms^-1, distance: {}m", time, acceleration, velocity, distance_from_sun);
-#
-#         time += delta_t;
-#     }
-# }
